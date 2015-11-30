6ced6d2f30db10b4c9e1fac7783039b0b65d1f0b
imrokas <rokas@imrokas.com>
2015-11-30 16:18:15 -0500

    implemented Grub class

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,16 +1,3 @@
 var RetiredForagerBee = function() {
-	ForagerBee.call(this);
-	this.age = 40;
-	this.job = 'gamble';
-	this.canFly = false;
-	this.color = 'grey';
 };
-RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
-RetiredForagerBee.prototype.constructor = RetiredForagerBee;
-ForagerBee.prototype.forage = function ( treasure ) {
-	this.treasureChest.push( treasure );
-	return 'I am too old, let me play cards instead';
-};
-ForagerBee.prototype.gamble = function ( treasure ) {
-	this.treasureChest.push( treasure );
-};
+
